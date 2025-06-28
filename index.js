01f5de0ccd3e782946fbbcfa839d5f83a82a114e
PATEL JENISH KANTILAL <jenishkp07@gmail.com>
2025-06-29 05:11:36 +0530

    update URI

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 //connect to mongoose
 
-mongoose.connect('mongodb+srv://jenishkp07:jenishkp07@cluster0.4neyt.mongodb.net/evivault?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('URI)
 .then(()=>console.log("MongoDB connected"))
 .catch(err=>console.log("MongoDB connection error :",err));
 
